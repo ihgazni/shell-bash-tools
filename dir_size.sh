9a4326b7ab97b9201d31cb52d86da4bcdbf8ad32
dapeli <terryinzaghi@163.com>
2019-09-26 14:54:53 +0800

    Update dir_size.sh

--- dir_size.sh
+++ dir_size.sh
@@ -1,1 +1,3 @@
 ls  -l | awk {'print $9'} | xargs  du -sh | sort -u | egrep "[0-9]+G"
+ls  -l | awk {'print $9'} | xargs  du -sh | sort -u | egrep "[0-9]+M"
+ls  -l | awk {'print $9'} | xargs  du -sh | sort -u | egrep "[0-9]+K"
